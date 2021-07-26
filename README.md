34479e2afa4f4a041be8491203d2bdd8edb7f898
Viviane Ribeiro <vivy.saribeiro@gmail.com>
2021-07-26 13:54:51 -0300

    Update Commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# hc-desafio-2-gestao-de-cadastros
- Hiring Coders Desafio 2#
+# Hiring Coders - Desafio 2#: Gestão de Cadastros
+
+## 💻 GECOM - Gerenciador de Cadastros
+
+O projeto foi criado em função do desafio feito durante o programa Hiring Coders da Gama Academy. Consiste em construir um sistema de cadastro de clientes e produtos.
+
+Funcionalidades
+O usuário consegue cadastrar clientes e produtos conforme formulário de dados requeridos. Estes dados são então armazenados localmente com localStorage e uma mensagem de Sucesso é exibida.
+
+Entregáveis:
+
+Os dados de clientes, endereços e produtos devem estar devidamente estruturados
+Dados no localstorage. Código fonte no github com o arquivo README detalhando as funcionalidades da programação.
+
+🔗 **Link do desafio:** https://gecom-hc-desafio-2.netlify.app/
+
+**Screenshoot Desktop:** <br>
+
+![GECOM](assets/images/desktop-screenshoot.png 'GECOM')
+
+**Screenshoot Mobile:** <br>
+
+![GECOM](assets/images/mobile-screenshoot.png 'GECOM')
--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 function validationProducts() {
-  let idProduct = document.getElementById('product-id').value
-  let nameProduct = document.getElementById('product-name').value
-  let brandProduct = document.getElementById('product-brand').value
-  let priceProduct = document.getElementById('product-price').value
-  let quantityProduct = document.getElementById('product-quantity').value
+    let idProduct = document.getElementById('product-id').value;
+  let nameProduct = document.getElementById('product-name').value;
+  let brandProduct = document.getElementById('product-brand').value;
+  let priceProduct = document.getElementById('product-price').value;
+  let quantityProduct = document.getElementById('product-quantity').value;
 
-  if (idProduct == '') alert('Favor preencher todos os campos')
-  else if (nameProduct == '') alert('Favor preencher todos os campos')
-  else if (brandProduct == '') alert('Favor preencher todos os campos')
-  else if (priceProduct == '') alert('Favor preencher todos os campos')
-  else if (quantityProduct == '') alert('Favor preencher todos os campos')
+  if (idProduct == "") alert("Favor preencher todos os campos");
+  else if (nameProduct == "") alert("Favor preencher todos os campos");
+  else if (brandProduct == "") alert("Favor preencher todos os campos");
+  else if (priceProduct == "") alert("Favor preencher todos os campos");
+  else if (quantityProduct == "") alert("Favor preencher todos os campos");
   else
     registerProducts(
       parseInt(idProduct),
@@ -17,15 +17,15 @@ function validationProducts() {
       brandProduct,
       parseInt(priceProduct),
       parseInt(quantityProduct)
-    )
+    );
 }
 
-function registerProducts() {
-  let idProduct = document.getElementById('product-id').value
-  let nameProduct = document.getElementById('product-name').value
-  let brandProduct = document.getElementById('product-brand').value
-  let priceProduct = document.getElementById('product-price').value
-  let quantityProduct = document.getElementById('product-quantity').value
+function registerProducts(){
+    let idProduct = document.getElementById('product-id').value;
+  let nameProduct = document.getElementById('product-name').value;
+  let brandProduct = document.getElementById('product-brand').value;
+  let priceProduct = document.getElementById('product-price').value;
+  let quantityProduct = document.getElementById('product-quantity').value;
 
   let addNewProduct = {
     idProduct,
@@ -35,62 +35,118 @@ function registerProducts() {
     quantityProduct
   }
 
-  if (typeof Storage !== 'undefined') {
-    let products = localStorage.getItem('products')
-    if (products == null) products = []
-    else products = JSON.parse(products)
-    products.push(addNewProduct)
-    localStorage.setItem('products', JSON.stringify(products))
-    alert('Produto cadastrado com sucesso')
-  } else alert('A versão do navegador é incompativel com essa aplicação.')
+    if (typeof(Storage) !== "undefined") {
+        let products = localStorage.getItem("products");
+        if (products == null) products = [];
+        else products = JSON.parse(products);
+        products.push(addNewProduct);
+        localStorage.setItem("products",JSON.stringify(products))
+        alert("Produto cadastrado com sucesso")
 }
+
+else alert("A versão do navegador é incompativel com essa aplicação.")
+};
+
 
 function validationClients() {
-  let nameClient = document.getElementById('name-client').value
-  let emailClient = document.getElementById('email-client').value
-  let phoneClient = document.getElementById('phone-client').value
-  let adressClient = document.getElementById('adress-client').value
-  let cityClient = document.getElementById('city-client').value
-  let stateClient = document.getElementById('state-client').value
+  let nameClient = document.getElementById('name-client').value;
+  let emailClient = document.getElementById('email-client').value;
+  let phoneClient = document.getElementById('phone-client').value;
+  let addressClient = document.getElementById('address-client').value;
+  let cityClient = document.getElementById('city-client').value;
+  let stateClient = document.getElementById('state-client').value;
 
-  if (nameClient == '') alert('Favor preencher todos os campos obrigatórios')
-  else if (emailClient == '')
-    alert('Favor preencher todos os campos obrigatórios')
-  else if (adressClient == '')
-    alert('Favor preencher todos os campos obrigatórios')
-  else if (phoneClient == '')
-    alert('Favor preencher todos os campos obrigatórios')
-  else if (cityClient == '')
-    alert('Favor preencher todos os campos obrigatórios')
-  else if (stateClient == '')
-    alert('Favor preencher todos os campos obrigatórios')
-  else registerClients()
+  if (nameClient == "") alert("Favor preencher todos os campos");
+  else if (emailClient == "") alert("Favor preencher todos os campos");
+  else if (addressClient == "") alert("Favor preencher todos os campos");
+  else if (phoneClient == "") alert("Favor preencher todos os campos");
+  else if (cityClient == "") alert("Favor preencher todos os campos");
+  else if (stateClient == "") alert("Favor preencher todos os campos");
+
+  else registerClients();
 }
 
-function registerClients() {
-  let nameClient = document.getElementById('name-client').value
-  let emailClient = document.getElementById('email-client').value
-  let adressClient = document.getElementById('adress-client').value
-  let phoneClient = document.getElementById('phone-client').value
-  let cityClient = document.getElementById('city-client').value
-  let stateClient = document.getElementById('state-client').value
+function registerClients(){
+  let nameClient = document.getElementById('name-client').value;
+  let emailClient = document.getElementById('email-client').value;
+  let phoneClient = document.getElementById('phone-client').value;
+  let addressClient = document.getElementById('address-client').value;
+  let cityClient = document.getElementById('city-client').value;
+  let stateClient = document.getElementById('state-client').value;
 
   let addNewClient = {
     nameClient,
     emailClient,
-    adressClient,
     phoneClient,
+    addressClient,
     cityClient,
-    stateClient
+    stateClient,
   }
 
-  if (typeof Storage !== 'undefined') {
-    let client = localStorage.getItem('client')
-    if (client == null) cliente = []
+  if (typeof Storage !== "undefine") {
+    let client = localStorage.getItem("client")
+    if (client == null) client = []
     else client = JSON.parse(client)
     client.push(addNewClient)
-    localStorage.setItem('cliente', JSON.stringify(client))
-    alert('Cliente cadastrado com sucesso')
-    e.preventDefault()
-  } else alert('A versão do navegador é incompativel com essa aplicação.')
+    localStorage.setItem("client", JSON.stringify(client))
+    alert("Cliente cadastrado com sucesso")
+  } 
+  
+  else alert("A versão do navegador é incompativel com essa aplicação.")
+};
+
+function listProducts() {
+
+if (typeof(Storage) !== "undefined") {
+    let products = localStorage.getItem("products");
+    document.write("<h1>Produtos Cadastrados:</h1>")
+    if (products == null)
+        document.write("<h3>Ainda não há nenhum cliente cadastrado!</h3>");
+    else {
+
+        produts = JSON.parse(products)
+        products.forEach(product => {
+          document.write('<ul>')
+          document.write(`<li>Código: ${product.idProduct}</li>`)
+          document.write(`<li>Nome do produto: ${product.nameProduct}</li>`)
+          document.write(`<li>Marca: ${product.brandProduct}</li>`)
+          document.write(`<li>Preço: ${product.priceProduct}</li>`)
+          document.write(`<li>Quantidade: ${product.quantityProduct}</li>`)
+          document.write('</ul>')
+        })
+        
+        
+    }
+} 
+else alert("A versão do seu navegador é muito antiga. Por isso, não será possível visualizar o estoque!");    
+
+};
+
+
+function listClients() {
+
+    if (typeof(Storage) !== "undefined") {
+        let client = localStorage.getItem("client");
+        document.write("<h1>Clientes Cadastrados:</h1>")
+        if (client == null)
+            document.write("<h3>Ainda não há nenhum cadastro!</h3>");
+        else {
+            client = JSON.parse(client);
+            client.forEach(client => {
+                document.write("<ul>");
+
+                document.write(`<li>Nome Completo: ${client.nameClient}</li>`)
+                document.write(`<li>E-mail: ${client.emailClient}</li>`)
+                document.write(`<li>Celular: ${client.phoneClient}</li>`)
+                document.write(`<li>Endereço Completo: ${client.addressClient}</li>`)
+                document.write(`<li>Cidade: ${client.cityClient}</li>`)
+                document.write(`<li>Estado: ${client.stateClient}</li>`)
+
+               
+                document.write("</ul>");
+            });
+        }
+    } 
+    else alert("A versão do seu navegador não é compatível. Por isso, não será possível visualizar o conteúdo!");    
+    
 }
